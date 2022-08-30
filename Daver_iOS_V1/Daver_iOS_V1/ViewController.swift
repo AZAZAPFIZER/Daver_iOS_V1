e10e76b3c99d102014bd250bfe033f2ab7dcf7ab
김상은 <ksemms20@gmail.com>
2022-08-30 11:24:14 +0900

    프로젝트 생성

---
+++ Daver_iOS_V1/Daver_iOS_V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daver_iOS_V1
+//
+//  Created by 김상금 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Daver_iOS_V1/Daver_iOS_V1UITests/Daver_iOS_V1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Daver_iOS_V1UITestsLaunchTests.swift
+//  Daver_iOS_V1UITests
+//
+//  Created by 김상금 on 2022/08/30.
+//
+
+import XCTest
+
+class Daver_iOS_V1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
