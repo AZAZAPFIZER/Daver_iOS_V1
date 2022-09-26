fcd4dbd3d925647ab16e660f0d544c5d4294058b
김상은 <ksemms20@gmail.com>
2022-09-26 10:09:10 +0900

    ✨ UITextField 왼쪽 오른쪽 공간 && 이미지 추가하는 Ext 추가

--- Daver_iOS_V1/Source/Presentation/Util/Extension/UIKit/UITextField/UITextFieldExt.swift
+++ Daver_iOS_V1/Source/Presentation/Util/Extension/UIKit/UITextField/UITextFieldExt.swift
@@ -17,4 +17,33 @@ extension UITextField {
         border.borderWidth = width
         self.layer.addSublayer(border)
     }
+    
+    func leftSpace(_ space: CGFloat) {
+        let spacer = UIView()
+        spacer.pin.width(space).height(of: self)
+        leftView = spacer
+        leftViewMode = .always
+    }
+    func rightSpace(_ space: CGFloat) {
+        let spacer = UIView()
+        spacer.pin.width(space).height(of: self)
+        rightView = spacer
+        rightViewMode = .always
+    }
+    func addLeftImage(image: UIImage, space: CGFloat = 10) {
+        let leftImage = UIImageView(image: image)
+        let view = UIView()
+        view.addSubViews(leftImage)
+        leftImage.pin.left(space).vCenter()
+        self.leftView = view
+        self.leftViewMode = .always
+    }
+    func addRightImage(image: UIImage, space: CGFloat = 10) {
+        let rightImage = UIImageView(image: image)
+        let view = UIView()
+        view.addSubViews(rightImage)
+        rightImage.pin.right(space).vCenter()
+        self.rightView = view
+        self.rightViewMode = .always
+    }
 }
